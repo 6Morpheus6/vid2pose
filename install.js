33afcaa793de386ffeacefbe22266cc8b46a275c
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-12-29 15:28:03 +0100

    downgrade mediapipe
    Prevents: "ImportError: cannot import name 'solutions' from 'mediapipe'"

--- install.js
+++ install.js
@@ -34,7 +34,8 @@ module.exports = async (kernel) => {
           "pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu121/torch2.1/index.html",
           "pip install -U openmim",
           "mim install mmengine==0.10.5 mmdet==3.2.0 mmpose==1.3.2",
-          "pip install -r requirements.txt"
+          "pip install -r requirements.txt",
+          "pip install mediapipe==0.10.21"
         ]
       }
     }, {
