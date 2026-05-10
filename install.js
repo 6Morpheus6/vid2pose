8244bdbc644fab6425bb5f94387a454ecbc30bc0
6Morpheus6 <wanton-decay@gmx.net>
2026-05-10 10:33:18 +0200

    update
    - add bundle ai
    - use uv
    - remove fs.link
    - move torch and mmcv installation to torch.js

--- install.js
+++ install.js
@@ -1,53 +1,38 @@
-module.exports = async (kernel) => {
-  let cmd = "pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/cpu"
-  if (kernel.platform === 'darwin') {
-    if (kernel.arch === "arm64") {
-      cmd = "pip install torch torchaudio torchvision"
-    } else {
-      cmd = "pip install torch==2.1.2 torchaudio==2.1.2"
+module.exports = {
+  requires: {
+    bundle: "ai"
+  },
+  run: [{
+    method: "shell.run",
+    params: {
+      message: "git clone https://github.com/6Morpheus6/vid_2_pose app"
     }
-  } else {
-    if (kernel.gpu === 'nvidia') {
-      if (kernel.gpu_model && / 50.+/.test(kernel.gpu_model)) {
-        cmd = "pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
-      } else {
-        cmd = "pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 xformers --index-url https://download.pytorch.org/whl/cu121"
+  }, {
+    method: "script.start",
+    params: {
+      uri: "torch.js",
+      params: {
+        venv: "env",
+        path: "app"
       }
-    } else if (kernel.gpu === 'amd') {
-      cmd = "pip install torch==2.1.2 torchvision==0.16.2 torchaudio==2.1.2 --index-url https://download.pytorch.org/whl/rocm5.6"
-    } 
-  }
-  return {
-    "run": [{
-      "method": "shell.run",
-      "params": {
-        "message": "git clone https://github.com/candywrap/vid2pose app"
-      }
-    }, {
-      "method": "shell.run",
-      "params": {
-        "venv": "env",
-        "path": "app",
-        "message": [
-          cmd,
-          "pip install opencv-python gradio==3.50.2 moviepy==1.0.3",
-          "pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cu121/torch2.1/index.html",
-          "pip install -U openmim",
-          "mim install mmengine==0.10.5 mmdet==3.2.0 mmpose==1.3.2",
-          "pip install -r requirements.txt",
-          "pip install mediapipe==0.10.21"
-        ]
-      }
-    }, {
-      "method": "fs.link",
-      "params": {
-        "venv": "app/env"
-      }
-    }, {
-      "method": "notify",
-      "params": {
-        "html": "Go back to the dashboard and launch the app!"
-      }
-    }]
-  }
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      venv: "env",
+      path: "app",
+      message: [
+        "uv pip install opencv-python gradio==3.50.2 moviepy==1.0.3 wheel",
+        "uv pip install -U openmim",
+        "mim install mmengine==0.10.5 mmdet==3.2.0 mmpose==1.3.2 --no-build-isolation",
+        "uv pip install -r requirements.txt",
+        "uv pip install mediapipe==0.10.21"
+      ]
+    }
+  }, {
+    method: "notify",
+    params: {
+      html: "Go back to the dashboard and launch the app!"
+    }
+  }]
 }
