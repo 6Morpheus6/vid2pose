f0028c1ae0e82335dc858b6785f22532e05c184e
6Morpheus6 <wanton-decay@gmx.net>
2026-05-10 10:34:59 +0200

    update
    - add bundle ai

--- start.js
+++ start.js
@@ -1,25 +1,31 @@
 module.exports = {
-  "daemon": true,
-  "run": [{
-    "method": "local.set",
-    "params": {
-      "type": "{{input.type}}"
+  requires: {
+    bundle: "ai"
+  },
+  daemon: true,
+  run: [{
+    method: "local.set",
+    params: {
+      type: "{{input.type}}"
     }
   }, {
-    "method": "shell.run",
-    "params": {
-      "venv": "env",
-      "env": {
-        "PYTORCH_ENABLE_MPS_FALLBACK": "1"
+    method: "shell.run",
+    params: {
+      venv: "env",
+      env: {
+        PYTORCH_ENABLE_MPS_FALLBACK: "1"
       },
-      "path": "app",
-      "message": "python video2openpose2.py --pose_model={{local.type}}",
-      "on": [ { "event": "/https?:\/\/[0-9.:]+/", "done": true } ]
+      path: "app",
+      message: "python video2openpose2.py --pose_model={{local.type}}",
+      on: [{
+        event: "/https?:\/\/[0-9.:]+/",
+        done: true
+      }]
     }
   }, {
-    "method": "local.set",
-    "params": {
-      "url": "{{input.event[0]}}"
+    method: "local.set",
+    params: {
+      url: "{{input.event[0]}}"
     }
   }]
 }
